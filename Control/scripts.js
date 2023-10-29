8adbfe27ca7240255ef35656e9bc430e2ee35876
andrewsmr2 <meghanandrews@Meghans-MacBook-Air.local>
2023-10-29 14:08:01 -0400

    control

---
+++ Control/scripts.js
@@ -0,0 +1,38 @@
+$(".color-button").click(
+    function(){
+    $("body").toggleClass("light");
+    $(".homepage").toggleClass("light");
+    $(".alternate1").toggleClass("light");
+    $(".alternate2").toggleClass("light");
+    $(".sound-descriptions").toggleClass("light");
+    $("form").toggleClass("light");
+    $(".questions-container").toggleClass("light");
+    $(".font-selector").toggleClass("light");
+    $("#myAudio").toggleClass("light");
+  }
+  );
+
+  $(".reveal-scene1").click(
+    function(){
+        $(".bigsur").toggleClass("show");
+    }
+  );
+
+    $(".reveal-scene2").click(
+    function(){
+        $(".").toggleClass("show");
+    }
+  );
+
+  $(".reveal-scene3").click(
+    function(){
+        $(".bigsur").toggleClass("show");
+    }
+  );
+
+  $(".reveal-scene4").click(
+    function(){
+        $(".bigsur").toggleClass("show");
+    }
+  );
+
