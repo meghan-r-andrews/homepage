3cb6bd72f700f54bd072cad5eea98b80f7eb288f
andrewsmr2 <meghan.r.andrews@gmail.com>
2024-03-07 14:12:19 -0500

    plants- some edits/updates

--- final/javascript/scripts.js
+++ final/javascript/scripts.js
@@ -27,6 +27,10 @@ $( function() {
       $(".prev").toggleClass("dark");
       $(".next").toggleClass("dark");
       $("figcaption").toggleClass("dark");
+      $("locations").toggleClass("dark");
+      $("location-hover").toggleClass("dark");
+      $("#myBtn").toggleClass("dark");
+
 
     }
   );
@@ -55,4 +59,24 @@ $( function() {
     const image = document.getElementById('zoomedImage');
     image.classList.toggle('zoomed');
   }
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
   
